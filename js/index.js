6e2bf2bd61a22d869e7cfc5bab61662ff909f8cc
João Pedro Alves <jotalmeida007@hotmail.com>
2020-07-24 11:14:46 -0300

    insert names into array

--- js/index.js
+++ js/index.js
@@ -1,7 +1,35 @@
 window.addEventListener('load', start);
 
 var globalNames = ['Um', 'Dois', 'Tres', 'Quatro'];
+var inputName = null;
 
 function start() {
-  console.log('start');
+  inputName = document.querySelector('#inputName');
+  preventFormSubmit();
+  activateInput();
+}
+
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(globalNames);
+  }
+  var form = document.querySelector('form');
+  form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput() {
+  function insertName(newName) {
+    globalNames.push(newName);
+  }
+
+  function handleTyping(event) {
+    if (event.key === 'Enter') {
+      insertName(event.target.value);
+    }
+  }
+
+  inputName.focus();
+  inputName.addEventListener('keyup', handleTyping);
 }
